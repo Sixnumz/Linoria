8a989ad20447f65ac13359a22daa0fbda491c9aa
Sixnumz <115585031+Sixnumz@users.noreply.github.com>
2023-01-27 18:37:25 +0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -29,8 +29,8 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 255, 255);
-    OutlineColor = Color3.fromRGB(0, 255, 255);
+    AccentColor = Color3.fromRGB(255, 255, 255);
+    OutlineColor = Color3.fromRGB(255, 255, 255);
 
     Black = Color3.new(0, 0, 0);
 
